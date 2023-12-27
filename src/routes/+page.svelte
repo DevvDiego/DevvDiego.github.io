2b69394caea978778dd1b38cbf14a5ebe73cf6f2
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2023-12-27 16:09:02 -0600

    corrected navbar semantics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,11 +6,11 @@
 
 <nav class="navbar bg-base-200">
     
-    <button class="flex-1">
-        <div class="btn btn-ghost text-xl no-animation">
+    <div class="flex-1">
+        <button class="btn btn-ghost text-xl no-animation">
             DevvDiego
-        </div>
-    </button>
+        </button>
+    </div>
 
     <div class="flex-0 gap-2">
 
@@ -29,16 +29,16 @@
         </div> -->
 
         <!-- ? how to make the icon adapt itself for dark or light mode -->
-         <button class="btn btn-ghost">
+        <button class="btn btn-ghost">
             <a href="https://github.com/devvdiego" target="_blank">
                 <img src={iconGithub} alt="github icon" class="w-8 h-8">
             </a>
         </button>
-        
+
         <!-- ? maybe remove a tag and put div for a js page moving func -->
         <button class="btn btn-primary btn-outline">
             <a>
-                Contact me
+                Reach me
             </a>
         </button>
 
