e97db5caed4685f0616f780ebedbd51eafc1efc1
Diego ans <diegovarela.202005gmail.com>
2023-12-26 20:16:04 -0600

    initial commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,35 +1,13 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
-import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			// dev config
-			// pages: 'build',
-			// assets: 'build',
-			
-			//production config
-			pages: "docs",
-            assets: "docs",
-
-			paths: {
-				// change below to your repo name
-				base: "/sveltePortfolio"
-			},
-			//??????????? will this work still?
-			// hydrate the <div id="svelte"> element in src/app.html
-			target: "#svelte",
-
-			fallback: "404.html",
-			precompress: false,
-			strict: true
-		})
-	},
-
-
-  preprocess: vitePreprocess()
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
+
 export default config;
