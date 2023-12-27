fec1b0143041aa813f52c14c9e37aa544752a250
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2023-12-27 16:05:55 -0600

    prod version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
 
     <div class="flex-0 gap-2">
 
-        <div class="dropdown mr-52">
+        <!-- <div class="dropdown mr-52">
             <div tabindex="0" role="button" class="btn ">
               Theme
             </div>
@@ -26,21 +26,21 @@
               <li><input type="radio" name="theme-dropdown" class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Aqua" value="aqua"/></li>
             </ul>
 
-        </div>
+        </div> -->
 
         <!-- ? how to make the icon adapt itself for dark or light mode -->
-        <!-- <button class="btn btn-ghost">
+         <button class="btn btn-ghost">
             <a href="https://github.com/devvdiego" target="_blank">
                 <img src={iconGithub} alt="github icon" class="w-8 h-8">
             </a>
-        </button> -->
+        </button>
         
         <!-- ? maybe remove a tag and put div for a js page moving func -->
-        <!-- <button class="btn btn-primary btn-outline">
+        <button class="btn btn-primary btn-outline">
             <a>
                 Contact me
             </a>
-        </button> -->
+        </button>
 
     </div>
 
