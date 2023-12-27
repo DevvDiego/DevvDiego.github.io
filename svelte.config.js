60e0d69bade1db001a9f17bde418776847849082
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2023-12-27 15:35:53 -0600

    set preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,7 +16,9 @@ const config = {
 		paths:{
 			base:"" //no base cause working on root of github.io
 		}
-	}
+	},
+
+	preprocess: vitePreprocess()
 };
 
 export default config;
