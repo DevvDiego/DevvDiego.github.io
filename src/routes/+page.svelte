dd46b134fd03c1fc0b39e6419b95785a60960646
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2023-12-28 21:09:25 -0600

    add basic hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,32 @@
 </script>
 
 
+<main>
+    <div class="hero min-h-screen bg-base-100">
+        <div class="hero-content ">
+            <div>
+
+                <!--? maybe add image here -->
+
+                <h1 class="text-5xl font-bold">I'm Diego Varela</h1>
+                <p class="py-6 sm:w-6/12 ">
+                    a backend dev, creating solutions for day to day problems on my developer life.
+                    Interested in the Internet of Things and the world of embedded systems
+                </p>
+                
+                <div class="sm:my-4 flex flex-wrap flex-col sm:flex-row gap-4">
+                    <button class="w-5/12 sm:w-52 btn btn-primary">See my work</button>
+
+                    <button class="w-5/12 sm:w-52 btn btn-ghost btn-outline">Contact me</button>
+                </div>
+
+
+
+            </div>
+        </div>
+      </div>
+</main>
+
 
 <style>
 </style>
