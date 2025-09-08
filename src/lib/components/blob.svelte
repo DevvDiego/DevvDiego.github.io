1e6a84645fdb2409e84ed18f5b4b51a083b0df18
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-09-08 00:30:10 -0600

    Move elements to Lib folder

--- src/lib/components/blob.svelte
+++ src/lib/components/blob.svelte
@@ -13,7 +13,7 @@
         z-index: 2;
         /* animation: rotomove 20s infinite; */
         background-repeat: no-repeat;
-        opacity: .3;
+        /* opacity: .3; */
         
         filter: blur(50px);
         border-radius: 100%;
@@ -24,6 +24,19 @@
             311deg in oklab, 
             oklch(75% 0.27 32) 0%, oklch(79% 0.33 360) 100%
             )
+        ;
+    }
+
+    .bluish-gradient {
+        background-image: 
+            radial-gradient(
+                circle at center,
+                oklch(46.418% 0.22255 306.703) 0%, oklch(47.856% 0.2951 266.955) 100% 
+            )
+            /* linear-gradient(
+                311deg in oklab, 
+                oklch(47.856% 0.2951 266.955) 0%, oklch(46.418% 0.22255 306.703) 100%
+            ) */
         ;
     }
 </style>
