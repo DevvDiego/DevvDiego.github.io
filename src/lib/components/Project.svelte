f8058ae208d6708de817b3b61d2920bd79c2c806
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-09-16 22:26:51 -0600

    Add Project.svelte

---
+++ src/lib/components/Project.svelte
@@ -0,0 +1,48 @@
+<script>
+
+
+    let {
+        title = "Project", summary = "",
+        link = "", linkText = "",
+        src = "", alt = "",
+        tech = []
+    } = $props();
+
+
+
+
+
+</script>
+
+
+
+
+<section class="pt-10">
+
+    <div class="title pb-4 flex flex-row flex-nowrap justify-between items-baseline">
+        <h3 class="text-3xl">{title}</h3>
+        <!-- change anchor text for an icon -->
+        <a class="text-lg" href={link}>{linkText}</a>
+    </div>
+
+    <div class="summary
+        text-base font-normal
+        pb-4
+    ">
+        <p>{summary}</p>
+    </div>
+
+    <div>
+        <img {src} {alt}>
+    </div>
+
+</section>
+
+
+
+
+<style>
+
+
+
+</style>
