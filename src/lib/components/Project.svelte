7d32cfef78a56a55ce4b26eea1acd9e61921c80c
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-09-18 01:59:19 -0600

    Add tech badge components into the project cards

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -1,15 +1,12 @@
 <script>
-    import Icon from "./Icon.svelte";
-    import Image from "./Image.svelte";
+    import Badge from "./Badge.svelte";
 
     let {
+        href,
+        imageSrc, imageAlt,
         title = "Project", summary = "",
+        techs = []
 
-        iconSrc, iconAlt, href,
-
-        imageSrc = "", imageAlt = "",
-
-        tech = []
     } = $props();
 
 </script>
@@ -20,27 +17,51 @@
 
 
 
-<div class="max-w-sm p-5 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-        <a {href}>
-            <img class="rounded-t-lg" src={imageSrc} alt="" />
-        </a>
+<div class="max-w-sm mb-28 rounded-lg bg-zinc-900/50">   
+    
+    <a {href}>
+        <img class="rounded-t-lg" src={imageSrc} alt={imageAlt} />
+    </a>
 
+    <div class="p-5">
+        
+        
+        
         <a {href}>
             <h5 class="my-2 text-3xl font-bold text-white">
                 {title}
             </h5>
         </a>
 
-        <p class="mb-3 font-normal text-gray-400">
+        <p class="font-normal text-gray-400">
             {summary}
         </p>
-        
+
+        <div class="badges py-4">
+
+            {#each techs as item}
+                <Badge name={item.name} width="18px" height="18px">
+                
+                    <!-- Render element as any other snippet -->
+                    {@render item.icon()}
+                
+                </Badge>
+            {/each}
+
+        </div>
+
+
         <a {href} class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-blue-900">
             Read more
             <svg class="w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
         </a>
+
+
+
+    </div>
+
 </div>
 
 
