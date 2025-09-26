f930411d6e0a6411c51a7b2f023559733947243d
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-09-26 00:25:25 -0600

    Add basic error management

--- public/index.php
+++ public/index.php
@@ -6,21 +6,33 @@ use Slim\Factory\AppFactory;
 use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
+use App\Database\Database;
+
 $app = AppFactory::create();
 
 
-
-
+$app->addErrorMiddleware(true, false, false);
 
 
 $app->get('/blog/{id}', function (Request $request, Response $response, array $args) {
 
     $id = $args["id"];
 
+    $post = Database::getInstance()->query(
+        "SELECT * FROM posts WHERE id = ?", 
+        [$id]
+    )->fetch();
+    
+    $response->getBody()->write(
+        "Titulo: " . $post["title"] . "\n" .
+        "Slug: " . $post["slug"]
+    );
 
-    $response->getBody()->write("hello from slim");
+
+    $response->getBody()->write(json_encode($post));
+
+
     return $response;
-
 });
 
 $app->run();
