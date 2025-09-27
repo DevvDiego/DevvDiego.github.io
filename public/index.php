b3d18f13160d51b0d6bf4ea65720b8c15c45b2cb
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-09-27 01:56:34 -0600

    Add CORS middleware

--- public/index.php
+++ public/index.php
@@ -13,6 +13,28 @@ $app = AppFactory::create();
 
 $app->addErrorMiddleware(true, false, false);
 
+// CORS middleware
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
+
+
+/* 
+
+Add pagination capabilities 
+
+Add rate limiting via server (!IMPORTANT)
+
+Add real verification of posts later
+
+*/
+
 
 
 $app->get('/blog', function (Request $request, Response $response){
