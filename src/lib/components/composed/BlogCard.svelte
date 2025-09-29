b72992760b493eb565e90f872cf3774d158ac067
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-09-28 18:41:29 -0600

    Change: use the minimal info provided by api

--- src/lib/components/composed/BlogCard.svelte
+++ src/lib/components/composed/BlogCard.svelte
@@ -1,8 +1,8 @@
 <script>
-
+    import { base } from "$app/paths";
+    
     let {
-        href,
-        title = "Project", summary = "",
+        slug, title, summary, date, readtime,
         class: klass
     } = $props();
 
@@ -12,21 +12,26 @@
     bg-zinc-800 rounded-xl
     border border-zinc-700
 ">
+    
+    <a href={ base + "/blog/" + {slug} }>
+        <div class="p-6">
 
-    <div class="p-6">
+            <h3 class="text-xl font-bold mb-2">
+                {title}
+            </h3>
 
-        <h3 class="text-xl font-bold mb-2">
-            {title}
-        </h3>
+            <p class="text-zinc-400 md:text-lg mb-4">
+                {summary}
+            </p>
+            
 
-        <p class="text-zinc-400 md:text-lg mb-4">
-            {summary}
-        </p>
-        
-        <a {href} class="inline-block mt-6 text-blue-400 font-medium hover:text-blue-300 transition-colors">
-            Ver mas
-        </a>
+            <!-- Maybe add a button even though clicking anywhere works -->
 
-    </div>
+            <!-- <a {href} class="inline-block mt-6 text-blue-400 font-medium hover:text-blue-300 transition-colors">
+                Ver mas
+            </a> -->
+
+        </div>
+    </a>
 
 </article>
