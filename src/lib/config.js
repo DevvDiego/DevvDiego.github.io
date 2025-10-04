b24972ac2f2f940a6d8e9db61d36d5ad15ffe523
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-10-03 18:10:54 -0600

    Change: base route route requests

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,6 +1,6 @@
 export const config = {
     // Desarrollo
-    API_URL: "http://localhost:8000"
+    API_URL: "http://localhost"
     
     // Producción
     /* API_URL: "https://api-devvdiego.infinityfreeapp.com" */
