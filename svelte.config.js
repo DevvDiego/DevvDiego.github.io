28ae5ef0395d02dfbbc8bad032de47e5bc874c63
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-10-03 18:04:42 -0600

    Add static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: '../public', // Directorio de salida (relativo al proyecto)
+			assets: '../public',
+			fallback: 'index.html',
+		}),
+
+		paths:{
+			base:"",
+		}		
 	}
+
 };
 
 export default config;
