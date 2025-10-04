cc92a37252d935ec7753e3c68e70b2b5de2d15b8
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-10-04 13:33:03 -0600

    Update config file to link the curren in use URLs

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,7 +1,11 @@
 export const config = {
     // Desarrollo
-    /* API_URL: "http://localhost:8000/api" */
+    /* 
+    API: "http://localhost/api",
+    BLOG: "http://localhost" */
+
     
     // Producción
-    API_URL: "https://api-devvdiego.infinityfreeapp.com/api"
+    API: "https://api-devvdiego.infinityfreeapp.com/api",
+    BLOG: "https://api-devvdiego.infinityfreeapp.com"
 }
