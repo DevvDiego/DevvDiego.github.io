01da1837548298c21cd1d35c00ae15f271924867
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-10-05 21:49:19 -0600

    Fix input not using correct type variable

--- frontend/src/lib/components/base/Input.svelte
+++ frontend/src/lib/components/base/Input.svelte
@@ -22,7 +22,7 @@
 </label>
 <input
     class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-    type="text"
+    {type}
     id="slug"
     {placeholder}
     bind:value
