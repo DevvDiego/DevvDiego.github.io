46afaaf484fee45e00569ccbf41cdaf269753631
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-10-05 23:58:58 -0600

    Add disabled option for the form components

--- frontend/src/lib/components/base/Select.svelte
+++ frontend/src/lib/components/base/Select.svelte
@@ -2,7 +2,8 @@
     let {
         value = $bindable(),
         // other props
-        id, label, children
+        id, label, isRequired = false, isDisabled = false,
+        children
     } = $props();
 
 </script>
@@ -12,7 +13,7 @@
     {label}
 </label>
 <select 
-    {id} bind:value 
+    {id} bind:value disabled={isDisabled} required={isRequired}
     class="
         w-full px-3 py-2 border border-gray-300 rounded-md
         focus:outline-none focus:ring-2 focus:ring-blue-500
--- frontend/src/lib/components/base/Textarea.svelte
+++ frontend/src/lib/components/base/Textarea.svelte
@@ -5,7 +5,7 @@
         // Accept an oninput function prop
         oninput,
         // other props
-        type, id, label, rows, isRequired = false
+        type, id, label, rows, isRequired = false, isDisabled = false,
     } = $props();
 
 </script>
@@ -15,6 +15,6 @@
     {label}
 </label>
 <textarea 
-    {id} bind:value {rows} required={isRequired} 
+    {id} {rows} bind:value disabled={isDisabled} required={isRequired}
     class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
 ></textarea>
