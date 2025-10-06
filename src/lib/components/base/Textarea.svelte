2625e1f92f591f2c1576436a39cde9aeb9313b81
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-10-05 21:35:31 -0600

    Add basic form components

---
+++ src/lib/components/base/Textarea.svelte
@@ -0,0 +1,20 @@
+<script>
+
+    let {
+        value = $bindable(),
+        // Accept an oninput function prop
+        oninput,
+        // other props
+        type, id, label, rows, isRequired = false
+    } = $props();
+
+</script>
+
+
+<label for="content" class="block text-sm font-medium text-gray-700">
+    {label}
+</label>
+<textarea 
+    {id} bind:value {rows} required={isRequired} 
+    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+></textarea>
