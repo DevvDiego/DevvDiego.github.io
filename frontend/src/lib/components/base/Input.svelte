5af8af7cec4e9cb554e5b38ebbf93bfa151b4d2a
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-10-05 21:35:31 -0600

    Add basic form components

---
+++ frontend/src/lib/components/base/Input.svelte
@@ -0,0 +1,31 @@
+<script>
+    let {
+        value = $bindable(),
+        // Accept an oninput function prop
+        oninput,
+        // other props
+        type, id, label, placeholder, isRequired = false
+    } = $props();
+
+    // Local function to handle the input event
+    function handleInput(event) {
+        // Update the local 'slug' value
+        value = event.target.value;
+        // Call the parent's oninput handler if provided
+        oninput?.(event);
+    }
+
+</script>
+
+<label for={id} class="block text-sm font-medium text-gray-700">
+    {label}
+</label>
+<input
+    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+    type="text"
+    id="slug"
+    {placeholder}
+    bind:value
+    oninput={handleInput}
+    required={isRequired}
+/>
---
+++ frontend/src/lib/components/base/Select.svelte
@@ -0,0 +1,25 @@
+<script>
+    let {
+        value = $bindable(),
+        // other props
+        id, label, children
+    } = $props();
+
+</script>
+
+
+<label for={id} class="block text-sm font-medium text-gray-700">
+    {label}
+</label>
+<select 
+    {id} bind:value 
+    class="
+        w-full px-3 py-2 border border-gray-300 rounded-md
+        focus:outline-none focus:ring-2 focus:ring-blue-500
+        text-gray-400
+    "
+>
+
+    {@render children?.()}
+
+</select>
