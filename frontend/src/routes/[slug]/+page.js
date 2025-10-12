95d0e41e0ff8f5a88d0cefa365dff843dda3440d
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-10-12 16:16:20 -0600

    Fix: blog post data not being passed correctly, destructure data.post

--- frontend/src/routes/[slug]/+page.js
+++ frontend/src/routes/[slug]/+page.js
@@ -4,21 +4,28 @@ import { config } from '$lib/config';
 /** @type {import('./$types').PageLoad} */
 export async function load({ params, fetch }) {    
 
-        const response = await fetch(`${config.API}/blog/` + params.slug);
+    const response = await fetch(`${config.API}/blog/` + params.slug);
 
-        if( !response.ok ){
+    if( !response.ok ){ 
+        error(404, `Route: "${params.slug}" was not found.`);
 
-            error(
-                404, 
-                `Route: "${params.slug}" was not found.`
-            )
+    }
 
-        }
+    let post = await response.json();
 
-        let post = await response.json();
+    //only handle the parsing, everything else is managed by sveltekit
+    try {
+        post.content =  JSON.parse(post.content);
+        
+    } catch (err) {
+        post.content = [{type:"subtitle",text:"Error cargando el contenido, reporta este error."}]
 
-        return {
-            post
-        }
+    }
+
+
+    return {
+        post
+    }
+
 
 }
