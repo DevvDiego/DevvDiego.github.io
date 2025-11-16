61fbcc089d3f98f958411a925f5437c33705a61b
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-11-16 17:45:45 -0600

    Add Label component

---
+++ frontend/src/lib/components/base/Label.svelte
@@ -0,0 +1,11 @@
+<script>
+
+    let {
+        id, text, class: klass, children
+    } = $props();
+
+</script>
+
+<label for={id} class="block text-sm font-medium text-gray-500 {klass}">
+    {@render children?.()}
+</label>
