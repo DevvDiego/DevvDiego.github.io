eda2fb9f9161aa7dd4d27aebe4d5b9cccbda52cf
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-11-16 20:20:25 -0600

    Add class editable prop for the Select component

--- src/lib/components/composed/form/Select.svelte
+++ src/lib/components/composed/form/Select.svelte
@@ -6,7 +6,7 @@
         value = $bindable(),
         // other props
         id, label, isRequired = false, isDisabled = false,
-        children
+        class:klass, children
     } = $props();
 
 </script>
@@ -21,7 +21,7 @@
     class="
         w-full px-3 py-2 border border-gray-300 rounded-md
         focus:outline-none focus:ring-2 focus:ring-blue-500
-        text-gray-400
+        text-gray-400 {klass}
     "
 >
 
