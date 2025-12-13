faf97f952344f633ade9914cc58289c2d5e018c8
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-12-13 00:18:18 -0600

    Change BriefCard buttons to serve a specific function

--- frontend/src/lib/components/composed/BriefCard.svelte
+++ frontend/src/lib/components/composed/BriefCard.svelte
@@ -8,7 +8,10 @@
         class: klass
     }: Post & ClassProp = $props();
 
-    function formatDate(date: string) {
+    let post: string = $derived(slug);
+
+
+    function formatDate(date: number | string | Date) {
         return new Date(date + 'T00:00:00').toLocaleDateString("es-ES", {
             year: 'numeric',
             month: 'short',
@@ -23,36 +26,34 @@
     border border-zinc-700
 ">
     
-    <a href={resolve("/[slug]", {slug})}>
-        <div class="p-6">
+    <div class="p-6">
 
-            <div class="flex items-center gap-2 text-sm text-zinc-400 mb-4">
-                <span>{formatDate(date)}</span>
-            </div>
-
-            <div>
-                <h3 class="text-lg font-bold mb-2">
-                    {title}
-                </h3>
-
-                <button class="
-                    cursor-pointer rounded-lg mt-4 mr-3 p-3 px-4
-                    inline-block font-medium transition-colors
-                    border border-white hover:bg-white hover:text-black
-                ">
-                    Ver mas
-                </button>
-
-                <button class="
-                    cursor-pointer rounded-lg mt-4 p-3 px-4
-                    inline-block font-medium transition-colors
-                    border border-neutral-950 hover:bg-neutral-950
-                ">
-                    Editar
-                </button>
-            </div>
-
+        <div class="flex items-center gap-2 text-sm text-zinc-400 mb-4">
+            <span>{formatDate(date)}</span>
         </div>
-    </a>
 
+        <div>
+            <h3 class="text-lg font-bold mb-2">
+                {title}
+            </h3>
+
+            <a href={resolve("/[slug]", {slug})} class="
+                cursor-pointer rounded-lg mt-4 mr-3 p-3 px-4
+                inline-block font-medium transition-colors
+                border border-white hover:bg-white hover:text-black
+            ">
+                Ver mas
+            </a>
+
+            <a href={resolve("/admin/edit/[post]", {post})} class="
+                cursor-pointer rounded-lg mt-4 p-3 px-4
+                inline-block font-medium transition-colors
+                border border-neutral-950 hover:bg-neutral-950
+            ">
+                Editar
+            </a>
+        </div>
+
+    </div>
+    
 </article>
