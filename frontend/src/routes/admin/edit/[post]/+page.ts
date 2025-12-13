b93a8d00482a75d493d2b4e772ab8b70d864817f
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-12-13 15:28:28 -0600

    Add load function for the editable post

---
+++ frontend/src/routes/admin/edit/[post]/+page.ts
@@ -0,0 +1,20 @@
+import { error } from '@sveltejs/kit';
+import { config } from '$lib/config';
+
+import type { PageLoad } from './$types';
+import type { Post } from '$lib/types.js';
+
+export const load: PageLoad = async ({ fetch, params }) => {    
+
+    const response = await fetch(`${config.API}/blog/` + params.post );
+
+    if( !response.ok ){ 
+        error(404, `Route: "${params.post}" was not found.`);
+
+    }
+
+    let post: Post = await response.json();
+
+    return { post }
+
+}
