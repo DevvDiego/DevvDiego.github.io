76012a7c3abe76a86d58e8838bdbddb6019614a6
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-12-15 16:54:04 -0600

    Implement JWT auth for the admin load function

--- frontend/src/routes/admin/+page.ts
+++ frontend/src/routes/admin/+page.ts
@@ -2,22 +2,25 @@ import { error } from '@sveltejs/kit';
 import { config } from '$lib/config';
 
 import type { PageLoad } from './$types';
-
+export const ssr = false; // Disable server-side rendering for this page
 
 export const load: PageLoad = async ({ fetch }) => {    
 
-    const loginData = {
-        password: ""
-    }
+    let stored_token = sessionStorage.getItem("blog_jwt");
+    if( stored_token === null ){ error(401, "Not authorized"); } // keep error, or redirect straight to home???
+    
+    const fetch_options: RequestInit = {
+        method: "GET", 
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${stored_token}`
+        }
+    };
 
-/*     const response = await fetch(`${config.API}/login`, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(loginData)
-    });
+    const response = await fetch(`${config.API}/admin/posts`, fetch_options);
 
-    if( !response.ok ){ error(403, `Could not log in`); }
+    if( !response.ok ){ error(401, "Not authorized"); }
 
-    let status = await response.json();
+    /* let status = await response.json();
     console.log(status) */
 }
