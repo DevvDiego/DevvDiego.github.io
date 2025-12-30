3f74ca3c587bdc404b3e6fcece5dbfc6815f9c3f
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-12-29 20:30:13 -0600

    Add comment about the usage of config.BLOG

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,8 +1,9 @@
 export const config = {
     API: import.meta.env.DEV 
-        ? "http://localhost/api" //dev
+        ? "http://localhost" //dev
         : "https://api-devvdiego.infinityfreeapp.com/api", //prod
         
+    //un used properties probably
     BLOG: import.meta.env.DEV
         ? "http://localhost" //dev
         : "https://api-devvdiego.infinityfreeapp.com" //prod
